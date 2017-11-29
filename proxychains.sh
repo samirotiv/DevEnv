60a57b8ba85fba9ce8495ce31fc901f70f4b6a85
samirotiv <samirotiv@gmail.com>
2017-11-29 07:13:43 +0000

    Proxychains script

---
+++ proxychains.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/rofl0r/proxychains-ng --depth 1 ~/git/proxychains-ng
+cd ~/git/proxychains-ng
+./configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+sudo make install-config
