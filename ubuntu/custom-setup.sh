8a51d8be0544857e57869f67bcf39d21f96eaea1
samir.otiv <samir.otiv@maximl.com>
2018-03-15 14:17:21 +0530

    Fix

--- ubuntu/custom-setup.sh
+++ ubuntu/custom-setup.sh
@@ -1,6 +1,6 @@
 # Vim Plug
 uname=$(whoami)
-sudo chown -R $(uname):$(uname) ~
+sudo chown -R $uname:$uname ~
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo chown -R $(uname):$(uname) ~
+sudo chown -R $uname:$uname ~
