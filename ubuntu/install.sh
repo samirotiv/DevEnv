864543573328fdb76fe280e932266464bd58defb
samirotiv <samirotiv@gmail.com>
2020-07-28 19:02:55 +0000

    Updated install script for focal

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,11 +4,11 @@ apt update && apt upgrade -y
 apt-get install software-properties-common curl -y
 
 # Add Neo Repo
-add-apt-repository ppa:neovim-ppa/stable -y
-apt-get update
+# add-apt-repository ppa:neovim-ppa/stable -y
+# apt-get update
 
 # Install NodeJS
-curl -sL https://deb.nodesource.com/setup_10.x -o /tmp/nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_12.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
 
 # Basics
@@ -17,7 +17,7 @@ apt-get install neovim -y
 ln -s /usr/bin/nvim /usr/local/bin/vim
 
 # Python Prerequisites
-apt-get install python-dev python-pip python3-dev python3-pip -y
+apt-get install python3-dev python3-pip -y
 pip3 install -U -r ./requirements3.txt
 
 # Node & Typescript
